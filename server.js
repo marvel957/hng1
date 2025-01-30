7ad092a265d364565abd6f5aaaad3ae4127f771a
marvel957 <hamsik957@gmail.com>
2025-01-30 22:15:40 +0100

    correct the port handling

--- server.js
+++ server.js
@@ -2,13 +2,17 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const port = 5000;
-const data = app.use(cors());
+const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.status(200).json({});
+  const data = {
+    email: "hamsik957@gmail.com",
+    current_datetime: new Date().toISOString(),
+    github_url: "https://github.com/marvel957/hngtask0",
+  };
+  res.status(200).json(data);
 });
 
-app.listen((port) => {
+app.listen(port, () => {
   console.log(`server running on port ${port}`);
 });
